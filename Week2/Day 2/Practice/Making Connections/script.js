be266771b29a27dbc862cfb71477121e1b90b036
oussama0710 <ouxavi80@gmail.com>
2023-01-24 18:21:33 +0100

    making connections

---
+++ Week2/Day 2/Practice/Making Connections/script.js
@@ -0,0 +1,12 @@
+var firstremove= document.querySelector(".Todd");
+var secondremove=document.querySelector(".Phil");
+function todRemove(){
+    firstremove.remove();
+}
+function phillRemove(){
+    secondremove.remove();
+}
+var changeName=document.querySelector(".card-body h1");
+function editProfile(){
+    changeName.innerText="Oussema Nouira"
+}
