f299b6a715c4a491a4a182718eb06e237ceceb2e
oussama0710 <ouxavi80@gmail.com>
2023-01-24 15:29:36 +0100

    Liking Likes

---
+++ Week2/Day 1/Core/Liking Likes/script2.js
@@ -0,0 +1,18 @@
+var Likep = document.querySelector("#one")
+var l=0
+function incLike(){
+    l++;
+    Likep.innerText=l+" like(s)";
+}
+var Likep2 = document.querySelector("#two")
+var l2=0
+function incLike2(){
+    l2++;
+    Likep2.innerText=l2+" like(s)";
+}
+var Likep3 = document.querySelector("#three")
+var l3=0
+function incLike3(){
+    l3++;
+    Likep3.innerText=l3+" like(s)";
+}
